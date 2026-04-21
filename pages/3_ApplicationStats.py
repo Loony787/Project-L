628d07dc050e802e13b4a70d13f7db383c881758
Loony <Almostfeedttv@gmail.com>
2026-04-21 17:23:57 +0200

    Application connected

--- pages/3_ApplicationStats.py
+++ pages/3_ApplicationStats.py
@@ -1,10 +1,65 @@
 import pandas as pd
 import streamlit as st
+import gspread
+
+#Website------------------------------------------------------------------------------------------------------------------------
 st.set_page_config(layout="wide",initial_sidebar_state="expanded")
 st.page_link("Portfolio.py", label="Home")
 
 if st.session_state.get('role') is None:
     st.switch_page("Portfolio.py")
 
-st.title("Applications")
-st.header("X")
+st.title("Applications",text_alignment='center')
+
+#Import--------------------------------------------------------------------------------------------------------------------------
+
+credentials = st.secrets["gcp_service_account"]
+gc = gspread.service_account_from_dict(credentials)
+sheet = gc.open("Project-J").sheet1
+
+#Basic preparations--------------------------------------------------------------------------------------------------------------
+
+data = sheet.get_all_records()
+df = pd.DataFrame(data)
+df = df.iloc[:,0].str.split(";", expand=True)
+df.columns = ["Company", "Status", "Rating", "Location", "Application Month", "IDC1", "IDC2", "IDC3", "IDC4", "IDC5", "IDC6"]
+df.index = range(1, len(df)+1)
+df.index.name = "No."
+df = df[df.iloc[:, 0] != ""]
+
+#st.dataframe(df)
+
+#Next Step-------------------------------------------------------------------------------------------------------------------------
+month_order = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+               "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+def BarChart():
+    df_Unique = df[["Status","Application Month"]].value_counts().reset_index()
+    #st.dataframe(df_Unique)
+    df_Unique["Application Month"] = pd.Categorical(df_Unique["Application Month"], categories= month_order, ordered=True)
+    st.bar_chart(df_Unique,x='Application Month',y='count',color='Status')
+
+#Metrics---------------------------------------------------------------------------------------------------------
+
+OA_Value = df[df["Status"] == "Pending"].shape[0]
+RA_Value = df[df["Status"] == "Rejected"].shape[0]
+NAE_Value = df[df["Status"] == "GameIsGone"].shape[0]
+INT_Value = df[df["Status"] == "Interviewed"].shape[0]
+OFF_Value = df[df["Status"] == "Offer"].shape[0]
+TA_Value = OA_Value + RA_Value + NAE_Value + INT_Value + OFF_Value
+
+
+def Absolute_METRICS():
+    col1, col2, col3= st.columns(3)
+    col1.metric(label= 'Total Applications', value= TA_Value)
+    col2.metric(label= 'Open Applications', value= OA_Value)
+    col3.metric(label= 'Rejected Applications', value= RA_Value)
+    col1.metric(label= 'Interviews', value= INT_Value)
+    col2.metric(label= 'Offers', value=OFF_Value)
+    col3.metric(label= 'No Answer Expected', value= NAE_Value,delta_description='*Application was 2 Months ago')
+
+#RUN-----------------------------------------------------------------------------------------------------------------
+st.divider()
+Absolute_METRICS()
+st.divider()
+BarChart()
+st.divider()
