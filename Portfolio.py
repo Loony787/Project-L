603e9d5e76025830c4e466cfdaa93a40ab7cc37c
Loony <Almostfeedttv@gmail.com>
2026-04-22 11:33:41 +0200

    Sleep2

--- Portfolio.py
+++ Portfolio.py
@@ -55,7 +55,7 @@ def Pop_Up():
         with st.spinner:
             time.sleep(3)
         st.session_state.role = 'RECRUITER'
-        st.rerun()
+        #st.rerun()
     elif guest:
         st.session_state.role = 'GUEST'
         st.rerun()
