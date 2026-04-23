08cc3b2c2c612db7b80fde838a3fd896362450b3
Loony <Almostfeedttv@gmail.com>
2026-04-23 15:13:57 +0200

    about time

--- pages/1_About.py
+++ pages/1_About.py
@@ -15,18 +15,19 @@ st.title("About",text_alignment="center")
 #Content------------------------------------------------------------------------------------------------
 
 def Content():
-    with st.expander("What is this about?",expanded=True):
+    col1, col2, col3 = st.columns([1,3,1])
+    with col2.expander("What is this about?",expanded=True):
         st.markdown("This website is a personal project. " \
                     "Its primary purpose was to build a small programming project, " \
                     "while also providing a way to showcase my skills to potential recruiters. " \
                     "I intentionally chose a project which I would actively use myself, rather than something purely technical like a to-do list.")
 
-    with st.expander("What does the roadmap look like?",expanded=True):
+    with col2.expander("What does the roadmap look like?",expanded=True):
         st.markdown("Once the core functionality is stable, I plan to analyze datasets that I find interesting, relevant and / or funny. " \
                     "This could include comparing football clubs in terms of market value or transfer balance relative to performance (wins / trophies / revenue). " \
                     "From a technical perspective, I aim to integrate APIs to enable automated data updates and more dynamic analyses.")    
 
-    with st.expander("What am I interested in?",expanded=True):
+    with col2.expander("What am I interested in?",expanded=True):
         st.markdown("I am particularly interested in analyzing data to derive insights from it and explain underlying patterns or differences. " \
                     "There is also a particular focus on identifying inefficiencies, especially in areas such as infrastructure, traffic and administrative processes, " \
                     "where significant optimization potential often exists. " \
