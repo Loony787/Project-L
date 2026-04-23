6cdde3818358f44e9c5d7284652cd025b47611a1
Loony <Almostfeedttv@gmail.com>
2026-04-23 23:32:06 +0200

    cooked

--- pages/2_Data Analytics.py
+++ pages/2_Data Analytics.py
@@ -26,9 +26,8 @@ def Visitor():
     df = pd.DataFrame(data)
     df.columns = ["Date", "Type"]
     st.dataframe(df)
-    #df_group = df.groupby(df["Date"].dt.date).size().reset_index(name='Visits')
-    #df_unique = df[['Date', 'Type']].value_counts().reset_index()
-    #st.line_chart(df_group, x='Date',y='Visits')
+    df_unique = df[['Date', 'Type']].value_counts().reset_index()
+    st.line_chart(df_unique, x='Date',y='count')
 #Content-----------------------------------------------------------------------------------------------------
 
 tab1, tab2, tab3 = st.tabs(["Visitors", "X", "X"])
