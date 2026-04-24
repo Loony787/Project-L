36aca6174fe8df23d2d84aa7db170264a1bcb43b
Loony <Almostfeedttv@gmail.com>
2026-04-24 13:03:31 +0200

    mh

--- pages/2_Data Analytics.py
+++ pages/2_Data Analytics.py
@@ -44,10 +44,7 @@ def VISITS():
     df['Date']= pd.to_datetime(df["Date"], format="%d.%m.%y")
     df_sort = df.sort_values(by='Date')
     st.dataframe(df_sort)
-    df_sort['Date']= pd.to_datetime(df_sort["Date"], format="%d.%m.%y")
     df_group = df_sort.groupby('Date').size().reset_index(name='Visits')
-    st.dataframe(df_group)
-
     df_group['Visits']= df_group['Visits'].cumsum()
     st.line_chart(df_group,x='Date', y='Visits')
     
