86dfa4a8ec6c6c2994494cbf47eaba288c9cdeef
Loony <Almostfeedttv@gmail.com>
2026-04-24 12:05:26 +0200

    format??

--- pages/2_Data Analytics.py
+++ pages/2_Data Analytics.py
@@ -41,7 +41,7 @@ def VISITS():
     col4.metric(label='Guest Visits', value= GUEST_VALUE, delta=GUEST_DELTA, delta_color='violet', delta_arrow='off')
     col5.metric(label='Friend Visits', value=FRIEND_VALUE, delta=FRIEND_DELTA, delta_color='violet', delta_arrow='off')
     
-    df["Date"] = pd.to_datetime(df["Date"])
+    df["Date"] = pd.to_datetime(df["Date"],"%d-%m-%Y")
     df_group = df.groupby('Date').size().reset_index(name='Visits')
     df_group['Visits']= df_group['Visits'].cumsum()
     st.line_chart(df_group,x='Date', y='Visits')
